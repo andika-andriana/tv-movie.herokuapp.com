299f4ad9638b8e471a05f8e5ae32c9f57a34767d
Andika Andriana <andriana.mia1@gmail.com>
2020-01-23 03:13:25 +0700

    Update Callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "tv-movie.auth0.com",
   clientID: "BOhnU6fuZHZwAjYg93Kne5xpdu5AmfiU",
-  callbackUrl: "http://localhost:3000/callback"
+  callbackUrl: "https://tv-movie.herokuapp.com/callback"
 };
